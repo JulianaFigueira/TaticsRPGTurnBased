85cf8712964c616a426c8bb2fc0f4c62d01f9b33
Juliana Figueira <juli.figueira@gmail.com>
2019-12-01 19:09:40 -0400

    CreateMap script and some assets

---
+++ Assets/_Map/CreateMapEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CreateMapScript))]
+public class CreateMapEditor : Editor
+{
+
+    public GameObject[] TileTypes;
+    public float NumberSides;
+    public float CenterDistance;
+    public float NumberTiles;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        CreateMapScript script = (CreateMapScript)target;
+        if (GUILayout.Button("Generate Map"))
+        {
+            script.Generate();
+        }
+    }
+}
---
+++ Assets/_Map/CreateMapScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMapScript : MonoBehaviour {
+
+    public GameObject[] TileTypes;
+    public float CenterDistance;
+    public int NumberTilesX;
+    public int NumberTilesZ;
+
+    GameObject[,] TileMap;
+
+    public Vector3 RotateXZ(Vector3 original, float angle)
+    {
+        float x = original.x;
+        float z = original.z;
+        float sn = Mathf.Sin(Mathf.Deg2Rad * angle);
+        float cs = Mathf.Cos(Mathf.Deg2Rad * angle);
+
+        float px = x * cs - z * sn;
+        float pz = x * sn + z * cs;
+
+        return new Vector3(px, original.y, pz);
+    }
+
+
+    public void Generate()
+    {
+        TileMap = new GameObject[NumberTilesX, NumberTilesZ];
+        Vector3 centerPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 nextDirection = new Vector3(0.0f, 0.0f, CenterDistance);
+
+        for (int i = 0; i < NumberTilesX; i++)
+        {
+            //make line
+            for (int k = 0; k < NumberTilesZ; k++)
+            {
+                TileMap[i,k] = Instantiate<GameObject>(TileTypes[Random.Range(0, TileTypes.Length)], centerPosition, Quaternion.identity, this.transform);
+                centerPosition += nextDirection;
+            }
+
+            //prepare next line
+            centerPosition = TileMap[i, 0].transform.position - RotateXZ(nextDirection, i%2 == 0 ? 60.0f : 120.0f);
+        }
+    }
+}
